a0b197958afd45dd4e97795cd359a0c3b3bace2b
Adriano Patrick Cunha <adrianopatrick@gmail.com>
2015-05-14 03:37:37 -0300

    [patrick.cunha] - alterado classe que manipula o xslx com os resutados

---
+++ prodin/src/main/java/br/unifor/ppgia/prodin/atualizacoes/Resultados.java
@@ -0,0 +1,56 @@
+package br.unifor.ppgia.prodin.atualizacoes;
+
+public class Resultados {
+	
+	private Integer numeroImunes;
+	private Integer numeroPseudoImunes;
+	private Integer numeroInfectantesGerados;
+	private Integer numeroDoentes;
+	private Integer numeroAcidentados;
+	private Integer numeroSadios;
+	private Integer nascimentos;
+	
+	public Integer getNumeroImunes() {
+		return numeroImunes;
+	}
+	public void setNumeroImunes(Integer numeroImunes) {
+		this.numeroImunes = numeroImunes;
+	}
+	public Integer getNumeroPseudoImunes() {
+		return numeroPseudoImunes;
+	}
+	public void setNumeroPseudoImunes(Integer numeroPseudoImunes) {
+		this.numeroPseudoImunes = numeroPseudoImunes;
+	}
+	public Integer getNumeroInfectantesGerados() {
+		return numeroInfectantesGerados;
+	}
+	public void setNumeroInfectantesGerados(Integer numeroInfectantesGerados) {
+		this.numeroInfectantesGerados = numeroInfectantesGerados;
+	}
+	public Integer getNumeroDoentes() {
+		return numeroDoentes;
+	}
+	public void setNumeroDoentes(Integer numeroDoentes) {
+		this.numeroDoentes = numeroDoentes;
+	}
+	public Integer getNumeroAcidentados() {
+		return numeroAcidentados;
+	}
+	public void setNumeroAcidentados(Integer numeroAcidentados) {
+		this.numeroAcidentados = numeroAcidentados;
+	}
+	public Integer getNumeroSadios() {
+		return numeroSadios;
+	}
+	public void setNumeroSadios(Integer numeroSadios) {
+		this.numeroSadios = numeroSadios;
+	}
+	public Integer getNascimentos() {
+		return nascimentos;
+	}
+	public void setNascimentos(Integer nascimentos) {
+		this.nascimentos = nascimentos;
+	}
+
+}
--- prodin/src/main/java/br/unifor/ppgia/prodin/poi/excel/XlsxFileManager.java
+++ prodin/src/main/java/br/unifor/ppgia/prodin/poi/excel/XlsxFileManager.java
@@ -1,14 +1,24 @@
 package br.unifor.ppgia.prodin.poi.excel;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+import br.unifor.ppgia.prodin.atualizacoes.Resultados;
 
 public class XlsxFileManager {
 
 	private static SXSSFWorkbook workbook = new SXSSFWorkbook(100);
+	private static Sheet sheet;
+	private static Integer row = 0;
+	private static Integer iteracao = 1;
+	private static String arquivoAtual = nomeProximoArquivo();
 
 	// pega o nome do arquivo com a maior sequencia dos experimentos já
 	// executados, e determina o nome do proximo arquivo com a proxima sequencia
@@ -25,6 +35,28 @@ public class XlsxFileManager {
 		return "experimento"
 				+ (Long.parseLong(files.get(files.size() - 1).getName()
 						.replaceAll("\\D", "")) + 1) + ".xslx";
+	}
+
+	public static void createSheet() {
+		sheet = workbook.createSheet("iteracao" + (iteracao++));
+	}
+
+	public static void inserirResultado(Resultados resultado) {
+		Row newRow = sheet.createRow(row++);
+		newRow.createCell(0).setCellValue(resultado.getNumeroImunes());
+		newRow.createCell(1).setCellValue(resultado.getNumeroPseudoImunes());
+		newRow.createCell(2).setCellValue(
+				resultado.getNumeroInfectantesGerados());
+		newRow.createCell(3).setCellValue(resultado.getNumeroDoentes());
+		newRow.createCell(4).setCellValue(resultado.getNumeroAcidentados());
+		newRow.createCell(5).setCellValue(resultado.getNumeroSadios());
+		newRow.createCell(6).setCellValue(resultado.getNascimentos());
+
+		try (FileOutputStream out = new FileOutputStream(arquivoAtual)) {
+			workbook.write(out);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
